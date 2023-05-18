eb047c3c69eba14e3615b331f954b1ef48a968f1
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2023-05-18 13:22:24 +0200

    deploy script

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,67 +1,12 @@
 import { Component } from 'react';
-import { Section } from './Section';
-import { ContactForm } from './ContactForm';
-import { Filter } from './Filter';
-import { ContactList } from './ContactList';
-import { nanoid } from 'nanoid';
 
 export class App extends Component {
-  state = {
-    contacts: [
-      { id: nanoid(), name: 'Gomer Simpson', number: '111-00-00' },
-      { id: nanoid(), name: 'Bard Simpson', number: '222-00-00' },
-      { id: nanoid(), name: 'Lisa Simpson', number: '333-00-00' },
-    ],
-    filter: '',
-  };
-
-  addContact = ({ name, number }) => {
-    const { contacts } = this.state;
-    const savedNamesList = contacts.map(contact => contact.name);
-
-    if (savedNamesList.includes(name)) {
-      return alert(`${name} is already in contacts`);
-    }
-
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { id: nanoid(), name, number }],
-    }));
-  };
-
-  deleteContact = currId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== currId),
-    }));
-  };
-
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
-    const contactFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(contactFilter)
-    );
-  };
+  state = {};
 
   render() {
-    const visibleContacts = this.getVisibleContacts();
-
     return (
       <>
-        <Section headTitle="Phonebook">
-          <ContactForm onSubmit={this.addContact} />
-        </Section>
-        <Section title="Contacts">
-          <Filter value={this.state.filter} onChange={this.handleChange} />
-          <ContactList
-            contacts={visibleContacts}
-            onDeleteContact={this.deleteContact}
-          />
-        </Section>
+        <p>Feedback React</p>
       </>
     );
   }
